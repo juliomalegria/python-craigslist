af46cb720784f9c18de4c08fddeb5a253d195192
Julio M. Alegria <juliomalegria@gmail.com>
2018-03-04 13:16:20 +0100

    Moved site_url as global in sites.py

--- craigslist/sites.py
+++ craigslist/sites.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-sites_url = 'http://www.craigslist.org/about/sites'
+ALL_SITES_URL = 'http://www.craigslist.org/about/sites'
+SITE_URL = 'http://%s.craigslist.org'
 
 
 def get_all_sites():
-    response = requests.get(sites_url)
+    response = requests.get(ALL_SITES_URL)
     response.raise_for_status()  # Something failed?
     soup = BeautifulSoup(response.content, 'html.parser')
     sites = set()
@@ -18,11 +19,11 @@ def get_all_sites():
 
     return sites
 
+
 def get_all_areas(site):
-    site_url = 'http://%s.craigslist.org/' % (site)
-    response = requests.get(site_url)
+    response = requests.get(SITE_URL % site)
     response.raise_for_status()  # Something failed?
     soup = BeautifulSoup(response.content, 'html.parser')
     raw = soup.select('ul.sublinks li a')
-    sites = set([a.attrs['href'].rsplit('/')[1] for a in raw])
+    sites = set(a.attrs['href'].rsplit('/')[1] for a in raw)
     return sites
