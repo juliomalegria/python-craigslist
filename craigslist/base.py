81075a382b2f674bb382a2e33692bbb8b29e8a01
Ira Horecka <ira89@icloud.com>
2020-12-18 20:16:06 -0800

    Link filter option with its numerical value
    Previously, filters from utils.get_list_filters returned a list of filters in <div class="search-attribute">. Numerical values that are tied to these filters (and ultimately concatenated to the query url) were assumed to increment by 1 from 1. This is not always the case, which in turn leads to an invalid filter code concatenated to the query url.
    
    These numerical codes are actually tied to their filters; therefore, this commit links the filter option (e.g. 'silver') with its numerical value (e.g. 8).

--- craigslist/base.py
+++ craigslist/base.py
@@ -95,14 +95,14 @@ class CraigslistBase(object):
                            list_filters[key])
                 if filter_['value'] is None:
                     parsed_filters[filter_['url_key']] = value
-                elif isinstance(filter_['value'], list):
+                elif isinstance(filter_['value'], dict):
                     valid_options = filter_['value']
                     if not hasattr(value, '__iter__') or isinstance(value, str):
                         value = [value]  # Force to list
                     options = []
                     for opt in value:
                         try:
-                            options.append(valid_options.index(opt) + 1)
+                            options.append(valid_options.get(opt))
                         except ValueError:
                             self.logger.warning(
                                 "'%s' is not a valid option for %s"
@@ -357,7 +357,7 @@ class CraigslistBase(object):
         attrs_after_colon = set(
             attr.split(': ', 1)[-1] for attr in result['attrs'])
         for key, options in iteritems(self.get_list_filters(self.url)):
-            for option in options['value']:
+            for option in list(options['value'].keys()):
                 if option in attrs_after_colon:
                     result[key] = option
                     break
@@ -423,5 +423,5 @@ class CraigslistBase(object):
         }
         list_filters = cls.get_list_filters(url)
         for key, options in iteritems(list_filters):
-            value_as_str = ', '.join([repr(opt) for opt in options['value']])
+            value_as_str = ', '.join(repr(opt) for opt in list(options['value'].keys()))
             print('* %s = %s' % (key, value_as_str))
